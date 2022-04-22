ae3105eee1f76c12cde12e2d94392b2a8867bb96
ktbatou <tbatoukaoutar@gmail.com>
2022-04-22 01:49:05 +0000

    updates in the rn_edit_text_material.xml

--- SignInButton.js
+++ SignInButton.js
@@ -4,6 +4,7 @@ import {View, Text, StyleSheet,TouchableOpacity} from 'react-native'
 
 export default function SignInButton()
 {
+    
     return (
         <TouchableOpacity>
             <View style={styles.buttonContainer}>
---
+++ checkSensor.js
@@ -0,0 +1,18 @@
+import FingerprintScanner from 'react-native-fingerprint-scanner'
+
+    FingerprintScanner
+                .isSensorAvailable()
+                .then(() => {
+                // Call the Fingerprint Scanner Method
+                    FingerprintScanner
+                        .authenticate({ description: 'Authenticate to access this' })
+                        .then(() => {
+                            // Method for Authentication
+                            onAuthenticate()
+                        })
+                        // Call error method
+                        .catch(error => onAuthenticationFailure(error))
+
+                })
+                 //Call the error method
+                .catch(error => onAuthenticationFailure(error))
